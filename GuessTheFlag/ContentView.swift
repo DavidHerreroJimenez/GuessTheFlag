7cbd6e049722fe0024c371926d8c4cd28b0a92d2
David Herrero <daheji@gmail.com>
2019-11-05 10:04:31 +0100

    View Composition was added

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -4,45 +4,68 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        
-        //        VStack(spacing: 20){
-        //            // max 10 elements
-        //        Text("Hello World")
-        //        Text("Hello World 2")
-        //        }
-//
-//        HStack(spacing: 20){
-//            // max 10 elements
-//            Text ("Hello World 0")
-//            Text("Hello World 1")
-//
-//        }
-        
-//        VStack{
-//            Text("1")
-//            Text("2")
-//            Spacer()
-//        }
-        
-//        ZStack{
-//            Text("Hello world")
-//            Text("This is inside a stack")
-//        }
-        
-        ZStack (alignment: .top){
-                    Text("Hello world")
-                    Text("This is inside a stack")
-                }
-        
-        
-        
-        
-        
 
-        
-        
+struct UsingStacksToArrangeViews: View {
+    var body: some View{
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Hello Wolrd")
+            HStack(spacing: 60){
+                Text("First text View")
+                
+                ZStack(){
+                    Text("<<")
+                    Text("Second Text View")
+                }
+            }
+            Spacer()
+        }
+    }
+}
+//struct ContentView: View {
+//    var body: some View {
+//
+//        //        VStack(spacing: 20){
+//        //            // max 10 elements
+//        //        Text("Hello World")
+//        //        Text("Hello World 2")
+//        //        }
+////
+////        HStack(spacing: 20){
+////            // max 10 elements
+////            Text ("Hello World 0")
+////            Text("Hello World 1")
+////
+////        }
+//
+////        VStack{
+////            Text("1")
+////            Text("2")
+////            Spacer()
+////        }
+//
+////        ZStack{
+////            Text("Hello world")
+////            Text("This is inside a stack")
+////        }
+//
+//        ZStack (alignment: .top){
+//                    Text("Hello world")
+//                    Text("This is inside a stack")
+//                }
+//
+//
+//
+//
+//
+//
+//
+//
+//    }
+//}
+
+struct ContentView: View{
+    var body: some View{
+        UsingStacksToArrangeViews()
     }
 }
 
